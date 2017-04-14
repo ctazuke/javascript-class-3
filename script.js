988ea79ed3f9300a1e2e6de0e2bc9c78af18d9d6
ctazuke <ctazuke@id.iit.edu>
2017-04-14 12:13:37 -0500

    examples of appendChild, innerText.

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
-document.write("Hello");
+// document.write("Hello");
 
-for (var i = 0; i < 10; i++) {
-  document.write("<br>");
-  document.write(i);
+// for (var i = 0; i < 10; i++) {
+//   document.write("<br>");
+//   document.write(i);
+// }
+
+// var j = 0;
+// do {
+//   document.write("<br>");
+//   document.write(j);
+//   j++;
+// } while(j < 10);
+
+// var k = 0;
+// while (k < 10) {
+//   document.write("<br>");
+//   document.write(k);
+//   k++;  
+// }
+
+
+
+
+var colorOfCellphone = document.createElement("h2");
+colorOfCellphone.innerText = "Silver";
+colorOfCellphone.style.color = "gray";
+document.body.appendChild(colorOfCellphone);
+
+var glassOrContactLens = document.createElement("h3");
+glassOrContactLens.innerText = "Contact Lenses";
+document.body.appendChild(glassOrContactLens);
+
+var favFruit = document.createElement("p");
+favFruit.innerText = "peach";
+favFruit.style.color = "pink";
+document.body.appendChild(favFruit);
+
+function addH1Element() {
+  var h1Element = document.createElement("h1");
+  h1Element.innerText = "Chieko Tazuke";
+  document.body.appendChild(h1Element);
+}
+addH1Element();
+
+function addASingleDevYo() {
+  var aSingleDivYo = document.createElement("div");
+  aSingleDivYo.className = "whatever";
+  document.body.appendChild(aSingleDivYo);  
 }
 
-var j = 0;
-do {
-  document.write("<br>");
-  document.write(j);
-  j++;
-} while(j < 10);
+// for (var t = 0; t < 4; t++) {
+//   addASingleDevYo();
+// }
 
-var k = 0;
-while (k < 10) {
-  document.write("<br>");
-  document.write(k);
-  k++;  
+// var t = 0;
+// do {
+//   addASingleDevYo();
+//   t++;
+// } while (t < 4);
+
+var t = 0;
+while (t < 4) {
+  addASingleDevYo();
+  t++;
 }
+
+
