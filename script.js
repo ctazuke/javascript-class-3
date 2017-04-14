1544f292fae1d9c7467e8ca25b228f1c31cb4dc1
ctazuke <ctazuke@id.iit.edu>
2017-04-14 11:02:54 -0500

    created do-while loop and while loop

---
+++ script.js
@@ -0,0 +1,20 @@
+document.write("Hello");
+
+for (var i = 0; i < 10; i++) {
+  document.write("<br>");
+  document.write(i);
+}
+
+var j = 0;
+do {
+  document.write("<br>");
+  document.write(j);
+  j++;
+} while(j < 10);
+
+var k = 0;
+while (k < 10) {
+  document.write("<br>");
+  document.write(k);
+  k++;  
+}
